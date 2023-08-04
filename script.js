befe4b9985756950b0ce5f351bedcbb5537fab0d
TychiqueMas <tychiquemas01@gmail.com>
2023-08-04 12:57:21 +0200

    change content script

--- script.js
+++ script.js
@@ -1,1 +1,55 @@
-console.log("ça fontionne");
+// // exo 1
+// let myAlphabet = [
+//     "A",
+//     "B",
+//     "C",
+//     "D",
+//     "E",
+//     "F",
+//     "G",
+//     "H",
+//     "I",
+//     "J",
+//     "K",
+//     "L",
+//     "M",
+//     "N",
+//     "O",
+//     "P",
+//     "Q",
+//     "R",
+//     "S",
+//     "T",
+//     "U",
+//     "V",
+//     "W",
+//     "X",
+//     "Y",
+//     "Z",
+//     "$",
+//     "*",
+//     "/",
+//     "-",
+//     "+",
+// ];
+
+// const testArr = ['bonjour', 'hello']
+
+// function myAlphabetLength(x) {
+//     console.log(x.length);
+
+// }
+
+// myAlphabetLength(myAlphabet);
+// myAlphabetLength(testArr);
+
+// // exo 2
+
+// const phones = ["Samsung", "Iphone", "Nokia", "Techno", "Huawei"]
+
+// phones.forEach(phone => console.log(phone));
+// phones.forEach((phone, index) => console.log(index));
+
+// exo 3
+
+const someDataTypes = ["Pomme", 1, true, 3]
